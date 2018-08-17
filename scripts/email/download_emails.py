69d94a6b63a370dc1e0ef10d4f1143c069bd3e38
Tom Phillips <11371361+tphillips619@users.noreply.github.com>
2018-08-16 17:49:42 -0700

    Update download_emails.py
    Changed BS4 parser to lxml.
    Modified parameters.json trial key
    Wrapped script into main()

--- scripts/email/download_emails.py
+++ scripts/email/download_emails.py
@@ -1,134 +1,236 @@
-import sys, os, re, json
-import imaplib
+from __future__ import division
+
+import os, re, json
+from email.parser import Parser
+from email.header import decode_header
+from bs4 import BeautifulSoup
 import pandas as pd
 
-PROJ_ROOT = os.environ['PROJ_ROOT']
-with open(PROJ_ROOT + 'parameters.json') as parameter_json:
-    params = json.load(parameter_json)
+PROJ_ROOT = os.environ.get('PROJ_ROOT')
+with open(PROJ_ROOT+'parameters.json') as json_file:
+    params = json.load(json_file)
 
-imap_params = params['IMAP']
+imap = params['IMAP']
+trial = params['trial']
 
-IMAP_SERVER = imap_params['IMAP_SERVER']
-EMAIL_FOLDERS = imap_params['EMAIL_FOLDERS']
-EMAIL_DOMAIN = imap_params['EMAIL_DOMAIN']
-PASSWORD = imap_params['PASSWORD']
-print(EMAIL_FOLDERS)
+DELIMETER = ','
+NEWLINE = os.linesep
 
-EMAIL_DIR = imap_params['EMAIL_DIR']
-print(EMAIL_DIR)
+print("Found the following files in the data directory")
+print(os.listdir(trial['EMAIL_DIR']))
 
-ACCOUNT_LIST = imap_params['ACCOUNT_LIST']
+ACCOUNT_LIST = imap['ACCOUNT_LIST']
+print("Found the following accounts in the ACCOUNT_LIST")
 print(ACCOUNT_LIST)
 
-re_gthread = re.compile('X-GM-THRID (\d*)')
-re_gmMsgid = re.compile('X-GM-MSGID (\d*)')
+# Text Content Regex
+phone_number_re = re.compile('\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})')
+google_maps_re = re.compile('https:\/\/maps.google.com\/\?q=((\w|\d|\.|\,)+\+)+((\w|\d|\.|\,)+)')
+trulia_listing_re = re.compile('www\.trulia\.com/rental/\d*')
 
-def extract_regex(string_to_search, compiled_regex):
-    match = compiled_regex.search(string_to_search)
-    if match:
-        return match.group(1)
+def extract_hyperlinks(soup):
+    link_set = set()
+    all_a = soup.findAll("a")
+
+    for link in all_a:
+        if 'href' in link.attrs:
+            if link.attrs['href'] != '#':   # Filter out empty '#' links
+                link_set.add(link.attrs['href'])
+
+    return list(link_set)
+
+def extract_regex(set_of_strings, compiled_regex):
+    match_set = set()
+
+    for link in set_of_strings:
+        m = compiled_regex.search(link)
+        if m: # if there is a regex match
+            match_set.add(m.group(0)) # add the match to the match_set
+
+    return list(match_set)
+
+def extract_single_regex(set_of_strings, compiled_regex):
+
+    for link in set_of_strings:
+        m = compiled_regex.search(link)
+        if m: # if there is a regex match
+            return m.group(0) # return it immediately
+
+    return ''
+
+def header_to_unicode_string(header):
+    str_builder = ''
+
+    decoded_tuples = decode_header(header)
+
+    for decoded_tuple in decoded_tuples:
+        if decoded_tuple[1]:
+            str_builder += unicode(decoded_tuple[0], decoded_tuple[1]) + " "
+        else:
+            str_builder += decoded_tuple[0]
+
+    return str_builder.strip()
+
+def make_soup(msg):
+    """
+    Input: msg, email.message.EmailMessage
+    Output: dict, {string feature_name: list feature_observation}
+    """
+    email = {feature:'' for feature in ['trulia','gmaps','tel','mailto','text_plain','text_html']}
+#     email['from'] = msg['From']
+    email['from'] = header_to_unicode_string(msg['From'])
+#     email['subject'] = msg['Subject']
+    email['subject'] = header_to_unicode_string(msg['Subject'])
+    email['date'] = pd.to_datetime(msg['date']) 
+    email['message-id'] = msg['Message-ID']
+
+    for part in msg.walk():  # dept-first traversal of multipart hierarchy
+        if part.get_content_type()=='text/plain':
+
+            charset = part.get_content_charset(failobj='utf-8')  # Set 'utf-8' as default
+            email['text_plain']= unicode(part.get_payload(decode=True),encoding=charset, errors='ignore')
+
+        elif part.get_content_type()=='text/html':
+
+            charset = part.get_content_charset(failobj='utf-8')  # Set 'utf-8' as default
+
+            part_payload = part.get_payload(decode=True) # Decode according to the Content-Transfer-Encoding header
+
+            if charset:
+                soup = BeautifulSoup(part_payload, 'lxml', from_encoding=charset)
+            else:
+                soup = BeautifulSoup(part_payload, 'lxml')
+
+            links = extract_hyperlinks(soup)
+            email['all_links'] = links
+            email['all_trulia'] = [link for link in links if re.search('trulia',link)]
+            email['trulia'] = extract_single_regex(links,trulia_listing_re)
+            email['gmaps'] = extract_regex(links, google_maps_re)
+            email['tel'] = [link.split(':')[1] for link in links if link.startswith('tel')]
+            email['mailto'] = [link.split(':')[1] for link in links if link.startswith('mailto')]
+
+            str_builder = ''
+            if soup.body:
+                for string in soup.body.stripped_strings:
+                    str_builder+=string + NEWLINE
+            else:
+                for string in soup.stripped_strings:
+                    str_builder+=string + NEWLINE
+            email['text_html'] = str_builder
+        else:
+            continue
+
+    # Select 'text/plain' only if navigable strings of 'text/html' is empty
+    if email['text_html']=='':
+        email['text']=email['text_plain']
+    else:
+        email['text']=email['text_html']
+
+    return email
+
+def get_text_alt(email):
+    with open(trial['EMAIL_DIR']+email, 'rb') as open_email:
+        msg = Parser().parse(open_email)
+    for part in msg.walk():
+        if part.get_content_type()=='text/html':
+            charset = part.get_content_charset(failobj='utf-8')  # Set 'utf-8' as default
+            part_payload = part.get_payload(decode=True) # Decode according to the Content-Transfer-Encoding header
+
+            if charset:
+                soup = BeautifulSoup(part_payload, 'lxml', from_encoding=charset)
+            else:
+                soup = BeautifulSoup(part_payload, 'lxml')
+
+    all_p = soup.findAll('p')
+
+    str_builder = ''
+    for p in all_p:
+        for string in p.stripped_strings:
+            str_builder+=string+NEWLINE
+    return str_builder
+
+# Convert lists contained in dataframe JSON encoded strings
+def list_to_json_string(single_list):
+    if single_list:
+        return json.dumps(single_list)
     else:
         return ''
 
-def download_folder(M, account_prefix, folder):
-    """
-    Download full content email and retrieve GMail IMAP extension identifiers.
-    Return a json containing ['email','X-GM-MSGID','X-GM-THRID']
-        X-GM-MSGID: Unique and immutable identifier for every email in GMail
-        X-GM-THRID: Thread ID for GMail
-        email:      String handle for e-mail on disk '<folder>/<account>.<num>.eml'
-        <num>:      Sequence number retrieved by IMAP.fetch()
-    """
-    
-    # Obtain sequence numbers of all emails contained in this mailbox folder
-    rv, seq_nums = M.search(None, "ALL")
-    #  e.g. ['1 2 3 4 5']
-    
-    if rv != 'OK':
-        print ("No messages found!")
-        return
-    
-    # List of dictionaries
-    email_ids = []
-    
-    email_prefix = folder + '/' + account_prefix + '.'
-    email_postfix = '.eml'
+def parse_all(response_type):
+    email_summary_cols = ['trulia', 'gmaps', 'tel', 'mailto', 'text']
+    all_emails = pd.DataFrame(columns=email_summary_cols)
 
-    for num in seq_nums[0].split():
-        email_id = {}
+    email_folder_dir = trial['EMAIL_DIR']+response_type
+    email_dict = {}
+    for email in os.listdir(email_folder_dir):
+        with open(os.path.join(email_folder_dir,email), 'rb') as open_email:
+            msg = Parser().parse(open_email)
+            email_dict[email] = make_soup(msg)
 
-        rv, email_identifiers = M.fetch(num, "(X-GM-MSGID X-GM-THRID)")
-        
-        if rv != 'OK':
-            print ("ERROR getting message", num)
-            return
-        else:
-            # M.fetch() returns unparsed tuple of strings. Parse after download
-            email_id['email'] = email_prefix + num + email_postfix
-            gthread_gmsgid_string = email_identifiers[0]
-            email_id['X-GM-THRID'] = extract_regex(gthread_gmsgid_string, re_gthread)
-            email_id['X-GM-MSGID'] = extract_regex(gthread_gmsgid_string, re_gmMsgid)
+    return_df = pd.DataFrame(email_dict).T
 
-            email_ids.append(email_id)
-            print("Processed email: " + str(num))
-            
-            # Download the entire e-mail and write to disk
-            rv, data = M.fetch(num, '(RFC822)')
-            if rv != 'OK':
-                print ("ERROR getting message", num)
-                return
-            else:
-                print ("Writing message " + email_id['email'])
-                f = open('%s/%s.eml' %(os.getcwd(), account_prefix+'.'+num), 'wb')
-                f.write(data[0][1])
-                f.close()
+    return_df['email']=return_df.index.values
 
-    return email_ids
+    #split off account name
+    return_df['account']=return_df['email'].apply(lambda x:x.split('.')[0])
+
+    #then prepend 'response-type/' to email_name.eml
+    return_df['email']=response_type+'/'+return_df['email']
+    return_df['race'] = return_df['account'].apply(lambda x:params['account_to_race'][x])
+
+    return return_df
+
 
 def main():
-    if not os.path.exists(EMAIL_DIR):
-        os.makedirs(EMAIL_DIR)
-    for folder in EMAIL_FOLDERS:
-        if folder in os.listdir(EMAIL_DIR):
-            print("Found "+folder+" in data directory")
-        else:
-            print("Making folder " + folder)
-            os.mkdir(EMAIL_DIR+folder)
+    responses = parse_all(trial['EMAIL_PREFIX'] + '-Response')
+    responses['response']=1
 
-    os.chdir(EMAIL_DIR)
-    print(os.getcwd())
+    nonresponses = parse_all(trial['EMAIL_PREFIX']+'-Nonresponse')
+    nonresponses['response']=0
 
-    email_id_list = []
-    
-    for account in ACCOUNT_LIST:
-        print(account)
-        email_account = account + EMAIL_DOMAIN
-        try:  
-            M = imaplib.IMAP4_SSL(IMAP_SERVER)
-            M.login(email_account, PASSWORD)
+    all_emails = pd.concat([responses,nonresponses], ignore_index=True, sort=True)
 
-            for folder in EMAIL_FOLDERS:
-                os.chdir(EMAIL_DIR+folder)
-                print(os.getcwd())
-                print("Processing mailbox: ", folder)
-                rv, data = M.select(folder)
+    all_emails['trulia_bool']=0
+    all_emails.loc[all_emails['trulia']!='','trulia_bool']=1
 
-                if rv == 'OK':
-                    email_id_list.extend(download_folder(M,account,folder))
-                    M.close()
-                else:
-                    print ("ERROR: Unable to open mailbox ", rv)
+    print("Looking for email_ids.csv in " + trial['EMAIL_DIR'])
+    email_ids = pd.read_csv(os.path.join(trial['EMAIL_DIR'],'email_ids.csv'))
 
-            M.logout()
-        except Exception:
-            raise
+    all_emails = pd.merge(all_emails, email_ids, on='email', how='left')
 
-    df_email_id = pd.DataFrame(email_id_list)
-    
-    out_columns = [u'email', u'X-GM-MSGID', u'X-GM-THRID']#, u'message_id']
-    df_email_id[out_columns].to_csv(EMAIL_DIR + "email_ids.csv", index=False)
+    print(all_emails.shape)
+    print(all_emails[['message-id','X-GM-THRID','X-GM-MSGID']].isnull().sum())
+    print(all_emails[all_emails['X-GM-THRID'].isnull()]['email'])
 
-    print("Wrote email ids to " + EMAIL_DIR + "email_ids.csv")
-    
+    ## Removing Inline CSS style from body
+    ## Parse Text separately when it appears like:
+    ##   @media only screen and (max-width: 550px), scr... 
+
+    # Filter for text
+    selection_list = all_emails[all_emails['text'].str.startswith('@')]['email'].values
+
+    # Transform filtered text
+    text_replacement_dict = {}
+    for email in selection_list:
+        text_replacement_dict[email]=get_text_alt(email)
+
+    # Overwite 'text' section with transformed text
+    for email in text_replacement_dict.keys():
+        all_emails.loc[all_emails['email']==email,'text'] = text_replacement_dict[email]
+
+
+    list_columns = ['all_links','all_trulia','gmaps','mailto','tel']
+
+    for col in list_columns:
+        all_emails[col] = all_emails[col].apply(list_to_json_string)
+
+    # Omit 'text_plain' and 'text_html' columns
+    out_columns = [u'all_links', u'all_trulia', u'date', u'from', u'gmaps', u'mailto',
+                   u'message-id', u'subject', u'tel', u'text', u'trulia', u'email', u'account',
+                   u'race', u'response', u'trulia_bool', u'X-GM-MSGID', u'X-GM-THRID']
+
+    # Write to disk
+    all_emails[out_columns].to_csv(trial['EMAIL_PARSED'],encoding='utf-8',index=False)
+
 if __name__ == "__main__":
     main()
