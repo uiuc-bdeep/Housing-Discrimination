ab4502db016d1b0538d670bd9049045ca3507390
cng10 <cng10@illinois.edu>
2019-06-09 17:13:49 -0500

    swap the order

--- scripts/listings_crawler/extract_sold_rental_data.py
+++ scripts/listings_crawler/extract_sold_rental_data.py
@@ -203,11 +203,11 @@ def extract_sold_rental_crime(driver, d):
 			button = driver.find_element_by_xpath("//*[@id='__next']/div/section/div[1]/div[2]/div[2]/div[2]/div/div/div[3]/button")
 		except:
 			try:
-				info_text = driver.find_element_by_xpath("//*[@id='main-content']/div[2]/div[2]/div[4]/div[2]/div/div/div[4]").text
-				button = driver.find_element_by_xpath("//*[@id='main-content']/div[2]/div[2]/div[4]/div[2]/div/div/div[4]")
-			except:
 				info_text = driver.find_element_by_xpath("//*[@id='main-content']/div[2]/div[2]/div[4]/div[2]/div/div/div[3]/div/div[2]").text
 				button = driver.find_element_by_xpath("//*[@id='main-content']/div[2]/div[2]/div[4]/div[2]/div/div/div[3]")
+			except:
+				info_text = driver.find_element_by_xpath("//*[@id='main-content']/div[2]/div[2]/div[4]/div[2]/div/div/div[4]").text
+				button = driver.find_element_by_xpath("//*[@id='main-content']/div[2]/div[2]/div[4]/div[2]/div/div/div[4]")
 
 	if "crime" in info_text.lower():
 		button.click()
