52665ca30669ed71cfaa9238a988977863abf7b1
Ubuntu <ubuntu@ip-172-31-25-26.ec2.internal>
2019-08-27 23:10:28 +0000

    updated for new layout

--- scripts/listings_inquirer/handle_listing_page.py
+++ scripts/listings_inquirer/handle_listing_page.py
@@ -76,6 +76,9 @@ def send_message(driver,name, email, phone_num, address,url,send):
         element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-mfegza-0.fVMZGj > div.HomeDetailsHero__Container-hubkl0-0.dBslmq > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.dodyUc > div.HomeDetailsHero__HomeStatusTitle-hubkl0-6.gZQsUf > span'
         error += try_page_element(driver,element,['OFF MARKET'])
 
+	element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.HomeDetailsHero__Container-hubkl0-0.dBslmq > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.dodyUc > div.HomeDetailsHero__HomeStatusTitle-hubkl0-6.gZQsUf > span > span'
+	error += try_page_element(driver,element,['OFF MARKET'])
+
 	element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-mfegza-0.fVMZGj > div.HomeDetailsHero__Container-hubkl0-0.bbKmZG > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.bHObIE > div.HomeDetailsHero__HomeStatusTitle-hubkl0-7.eLIcbC > span.PropertyTag-sc-5t90lx-0.ebIvTY.Tag__TagBase-sc-1rp6fz0-1.clFGBQ.Text__TextBase-sc-1cait9d-0.dJEjin > span'
 	error  += try_page_element(driver,element,['SOLD'])
 
